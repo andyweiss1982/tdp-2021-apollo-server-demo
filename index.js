cc2c0908bf384f6d3886706d7ec7d1d42dd06e88
Andy Weiss <andyweiss1982@gmail.com>
2021-10-01 08:27:19 -0600

    full crud functionality

--- index.js
+++ index.js
@@ -12,30 +12,45 @@ const typeDefs = gql`
 
   type Query {
     allTasks: [Task]!
+    Task(id: Int!): Task
   }
 
   type Mutation {
     createTask(description: String!): Task!
     toggleTaskCompletion(id: Int!): Task
+    deleteTask(id: Int!): Task
   }
 `;
 
 const resolvers = {
   Query: {
     allTasks: () => tasks,
+    Task: (_parent, { id }, _context, _info) => {
+      const task = tasks.find((t) => t.id === id);
+      return task;
+    },
   },
   Mutation: {
-    createTask: (_parent, { description }, _context) => {
+    createTask: (_parent, { description }, _context, _info) => {
       id += 1;
       const task = { id, description, completed: false };
       tasks.push(task);
       return task;
     },
-    toggleTaskCompletion: (_parent, { id }, _context) => {
+    toggleTaskCompletion: (_parent, { id }, _context, _info) => {
       const task = tasks.find((t) => t.id === id);
       if (task) {
         task.completed = !task.completed;
       }
+      return task;
+    },
+    deleteTask: (_parent, { id }, _context, _info) => {
+      const taskIndex = tasks.findIndex((t) => t.id === id);
+      if (taskIndex === -1) {
+        return;
+      }
+      const task = tasks[taskIndex];
+      tasks.splice(taskIndex, 1);
       return task;
     },
   },
